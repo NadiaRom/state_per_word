72c4f2d218adf830b9732c1203b1e4d3b4bfef14
Nadiia Romanenko <nadiia.romanenko@gmail.com>
2018-12-16 18:53:29 +0200

    added text spans

--- index.js
+++ index.js
@@ -2,15 +2,69 @@ const d3 = require('d3');
 const $ = require('jquery');
 const tippy = require('tippy.js');
 
-const svg_text = d3.select('svg#text_line');
+const canvas = d3.select('canvas#text_line');
 
 d3.json('ru_states_per_word.json')
     .then(function (data) {
-        debugger;
-    });
+        
+        for (const k of Object.keys(data)) {
+            data[k].states_per_word = data[k].states_per_word.slice(3);
+        }
+        
+        d3.select('nav')
+            .selectAll('button.sel_article')
+            .data(Object.keys(data))
+            .enter()
+            .append('button')
+            .classed('sel_article', true)
+            .text((d) => d);
+        
+        $('button.sel_article').first().addClass('active');
 
-svg_text
-    .attr('height',  '5000px')
-    .attr('width', function () {
-        return $(this).closest('main').width();
+        let active_doc = $('button.sel_article.active').text();
+
+        const draw_text = function () {
+            $('main article span').remove();
+            var spans = d3.select('main article')
+                .selectAll('span')
+                .data(data[active_doc].states_per_word)
+                .enter()
+                .append('span')
+                .text(d => d[0]);
+        };
+
+        draw_text();
+        canvas
+            .attr('height',  function () {
+                return $(this).closest('article').height();
+            })
+            .attr('width', function () {
+                return $(this).closest('article').width();
+            });
+
+        let span_coords = {};
+        const draw_canvas = function () {
+            let article_offset = $('main article').offset();
+            $('main article span').each(function () {
+                debugger;
+                $(this)
+            })
+        };
+
+
+        $('button.sel_article').click(function () {
+            $('button.sel_article').removeClass('active');
+            $(this).addClass('active');
+            active_doc = $('button.sel_article.active').text();
+            draw_text();
+
+            canvas
+                .attr('height',  function () {
+                    return $(this).closest('article').height();
+                })
+                .attr('width', function () {
+                    return $(this).closest('article').width();
+                });
+        })
+        
     });
