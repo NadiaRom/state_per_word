a8587cbb8d3273275be0c5e05f29899fe56ff1f4
Nadiia Romanenko <nadiia.romanenko@gmail.com>
2018-12-16 17:03:35 +0200

    text first

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const $ = require('jquery');
 const tippy = require('tippy.js');
 
 const svg_text = d3.select('svg#text_line');
+
+d3.json('ru_states_per_word.json')
+    .then(function (data) {
+        debugger;
+    });
+
 svg_text
     .attr('height',  '5000px')
     .attr('width', function () {
