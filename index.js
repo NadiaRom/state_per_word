c74bea924a3937649d560535b4e8aae2a3801283
Nadiia Romanenko <nadiia.romanenko@gmail.com>
2018-12-16 16:49:10 +0200

    data

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const d3 = require('d3');
+const $ = require('jquery');
+const tippy = require('tippy.js');
+
+const svg_text = d3.select('svg#text_line');
+svg_text
+    .attr('height',  '5000px')
+    .attr('width', function () {
+        return $(this).closest('main').width();
+    });
